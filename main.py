e5fa9c2c1951e6a8f2a1cca474ae7ef36b821af8
Marcelo Zapatta <mzapatta@insiderealestate.com>
2024-07-02 13:49:47 -0300

    feat: getting video data e aligments

---
+++ main.py
@@ -0,0 +1,97 @@
+import os
+# This is used to preproccess and import videos
+import cv2
+import tensorflow as tf
+import numpy as np
+from typing import List
+from matplotlib import pyplot as plt
+import imageio
+
+import matplotlib
+matplotlib.use('Agg')
+
+# library to download from google drive...
+import gdown
+
+# Function to load the alignment based on the path
+def load_alignments(path:str) -> List[str]:
+  with open(path, 'r') as f:
+    lines = f.readlines()
+  tokens = []
+  for line in lines:
+    line = line.split()
+    if line[2] != 'sil':
+      tokens = [*tokens, ' ', line[2]]
+  return char_to_num(tf.reshape(tf.strings.unicode_split(tokens, input_encoding='UTF-8'), (-1)))[1:]
+
+# Function to load video frames
+def load_video(path:str) -> List[float]:
+  # Load the video and get frame per frame
+  cap = cv2.VideoCapture(path)
+  frames = []
+  for _ in range(int(cap.get(cv2.CAP_PROP_FRAME_COUNT))):
+    ret, frame = cap.read()
+    # Change frame to grayscale
+    frame = tf.image.rgb_to_grayscale(frame)
+    # Isolate only the mouth at the frame
+    frames.append(frame[190:236,80:220,:])
+  cap.release()
+
+  mean = tf.math.reduce_mean(frames)
+  std = tf.math.reduce_std(tf.cast(frames, tf.float32))
+  return tf.cast((frames - mean), tf.float32) / std
+
+# Function to load both alignement and video frame
+def load_data(path:str):
+    path = bytes.decode(path.numpy())
+    # Grabing the filename by spliting the path and getting the last element on array
+    file_name = path.split('/')[-1].split('.')[0]
+    video_path = os.path.join('data', 's1', f'{file_name}.mpg')
+    alignment_path = os.path.join('data', 'alignments', 's1', f'{file_name}.align')
+    frames = load_video(video_path)
+    alignments = load_alignments(alignment_path)
+
+    return frames, alignments
+
+def mappable_function(path:str) -> List[str]:
+  result = tf.py_function(load_data, [path], (tf.float32, tf.int64))
+  return result
+
+# Here we are prevent exponential memory grow
+physical_devices = tf.config.list_physical_devices('GPU')
+try:
+  tf.config.experimental.set_memory_growth(physical_devices[0], True)
+except:
+  pass
+
+# Checks if the folder data already exists, if not download the dataset of videos
+if os.path.exists('./data') == False:
+  url = 'https://drive.google.com/uc?id=1YlvpDLix3S-U8fd-gqRwPcWXAXm8JwjL'
+  output = 'data.zip'
+  gdown.download(url, output, quiet=False)
+  gdown.extractall('data.zip')
+
+
+# All caracters that we are going to use
+vocab = [x for x in "abcdefghijklmnopqrstuvwxyz'?123456789"]
+
+char_to_num = tf.keras.layers.StringLookup(vocabulary=vocab, oov_token="")
+num_to_char = tf.keras.layers.StringLookup(
+  vocabulary=char_to_num.get_vocabulary(), oov_token="", invert=True
+)
+
+print(
+  f"The vocabulary is: {char_to_num.get_vocabulary()}"
+  f"(size = {char_to_num.vocabulary_size()})"
+)
+
+test_path = './data/s1/bbal6n.mpg'
+tf.convert_to_tensor(test_path).numpy().decode('utf-8').split('/')[-1].split('.')[0]
+frames, alignments = load_data(tf.convert_to_tensor(test_path))
+
+plt.figure(figsize=(10,10))
+test = plt.imshow(frames[40])
+plt.savefig('test.png')
+
+print(tf.strings.reduce_join([bytes.decode(x) for x in num_to_char(alignments.numpy()).numpy()]))
+
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Building a lipnet
+
+This is a tutorial from youtube on how to build your own lipnet, a deeplearning machine that are able to perform lip reading.
+
+## How to run
+
+#### Create the env
+
+``python3 -m venv .venv``
+
+#### Activate the env
+
+``source .vevn/bin/active``
+
+#### Install requirements
+
+``python3 -m pip install -r requirements.txt``
